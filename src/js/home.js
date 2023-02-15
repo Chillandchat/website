3806717c22ac67f46075dfbf0c0105fc3d7fc86b
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-02-15 22:15:29 +1100

    Update link

--- src/js/home.js
+++ src/js/home.js
@@ -2,12 +2,11 @@ function download() {
   var ERROR_LINK = "https://chillandchat-mobile.web.app/error.html";
   var ANDROID_LINK =
     "https://expo.dev/@cheng-alvin/chill-and-chat-mobile-app?serviceType=classic&distribution=expo-go";
+  var IOS_LINK = "https://apps.apple.com/au/app/chill-chat/id6443742617";
 
   if (navigator.userAgent.indexOf("Android") != -1)
     window.location = ANDROID_LINK;
 
-  if (navigator.userAgent.indexOf("like Mac") != -1)
-    // Will add app store link soon...
-    window.location = ERROR_LINK;
+  if (navigator.userAgent.indexOf("like Mac") != -1) window.location = IOS_LINK;
   else window.location = ERROR_LINK;
 }
