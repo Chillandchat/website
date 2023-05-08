278bc26231d7d37b19a40df8be510bf55a473d6b
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-05-08 21:25:37 +1000

    Changed default link to app store

--- src/js/home.js
+++ src/js/home.js
@@ -1,12 +1,9 @@
 function download() {
-  var ERROR_LINK = "https://chillandchat-mobile.web.app/error.html";
   var ANDROID_LINK =
     "https://expo.dev/@cheng-alvin/chill-and-chat-mobile-app?serviceType=classic&distribution=expo-go";
   var IOS_LINK = "https://apps.apple.com/au/app/chill-chat/id6443742617";
 
   if (navigator.userAgent.indexOf("Android") != -1)
     window.location = ANDROID_LINK;
-
-  if (navigator.userAgent.indexOf("like Mac") != -1) window.location = IOS_LINK;
-  else window.location = ERROR_LINK;
+  else window.location = IOS_LINK;
 }
